04c9b524f59584f2f51d7787d5dd5ac66cac90d8
jstzb <j.stolzenbach@student.uni-tuebingen.de>
2016-02-29 23:46:03 +0100

    added Logout,BadLoginMethod
    BadLoginMethod should fail authentication and show that application is secure.
    Logout Method will lead you to popup asking for reconfirmation of logout request.
    To DO: Selecting Yes in Reconfirmation, sadly XPATH doesnt work

---
+++ src/seleniumtest/java/BadLoginMethod.java
@@ -0,0 +1,37 @@
+package selenium;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BadLoginMethod {
+	public WebDriver driver;
+	public String baseUrl;
+	
+	@Before
+	public void setUp() throws Exception{
+		driver = new FirefoxDriver();
+		baseUrl="http://localhost:8080";
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	}
+	@Test
+	public void loginMethod() throws Exception {
+		driver.get(baseUrl+"/frontend.jsp");
+		driver.findElement(By.id("isc_V")).clear();
+		driver.findElement(By.id("isc_V")).sendKeys("noAdmin");
+	    driver.findElement(By.id("isc_Y")).clear();		//password
+	    driver.findElement(By.id("isc_Y")).sendKeys("noAdmin");
+	    driver.findElement(By.id("isc_1M")).click();	//submit
+	}
+	@After
+	public void tearDown(){
+		driver.quit();
+	}
+}
---
+++ src/seleniumtest/java/Logout.java
@@ -0,0 +1,44 @@
+package selenium;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Logout {
+	public WebDriver driver;
+	public String baseUrl;
+	public String username;
+	public String password;
+	
+	@Before
+	public void setUp(){
+		driver = new FirefoxDriver();
+		baseUrl="http://localhost:8080";
+		username="admin";
+		password="admin";
+		
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get(baseUrl+"/frontend.jsp");
+		driver.findElement(By.id("isc_V")).clear();
+		driver.findElement(By.id("isc_V")).sendKeys(username);
+	    driver.findElement(By.id("isc_Y")).clear();		//password
+	    driver.findElement(By.id("isc_Y")).sendKeys(password);
+	    driver.findElement(By.id("isc_1M")).click();	//submit
+	}
+	@Test
+	public void logout() throws Exception {
+		driver.findElement(By.name("isc_2Qicon")).click();
+	}
+	@After
+	public void tearDown(){
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.quit();
+	}
+}
