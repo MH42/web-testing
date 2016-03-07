54a98569de9d5e1194eee9df9ad80bde4146980e
johannesh <johanneshoefener@gmail.com>
2016-03-07 20:23:19 +0100

    Updated Testfilter

--- src/seleniumtest/java/tests/TestFilter.java
+++ src/seleniumtest/java/tests/TestFilter.java
@@ -9,21 +9,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import pageObjects.LoginPage;
+import pageObjects.SwitchTabs;
 
 public class TestFilter {
 	public LoginPage login;
 	public WebDriver driver;
+	public SwitchTabs switchtabs;
 	
 	 @Before
 	 public void setUp() throws Exception {
 		 login = new LoginPage(driver);
 		 login.loginAdmin();
 		 driver = login.driver;	
+		 switchtabs = new SwitchTabs();
+		 switchtabs.switchTabs("Documents", driver);	
 		 driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	 	  }
 
 	 @Test
-	public void testFilter(String p, WebDriver driver){
+	public void testFilter(){
 		driver.findElement(By.xpath("//img[contains(@src, 'http://localhost:8080/skin/images/filter.png')]")).click();
 		driver.findElement(By.id("isc_AZ")).clear();
 		driver.findElement(By.id("isc_AZ")).sendKeys("intro");;
