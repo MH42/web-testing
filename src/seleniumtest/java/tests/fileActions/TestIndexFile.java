2b0d08e7abc4297917f276768d74dc4bc9209c98
Jan Stolzenbach <j.stolzenbach@student.uni-tuebingen.de>
2016-03-08 18:23:22 +0100

    Tests for Filecontextmenu added

---
+++ src/seleniumtest/java/tests/fileActions/TestIndexFile.java
@@ -0,0 +1,45 @@
+package tests.fileActions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pageObjects.FileContextMenu;
+import pageObjects.LoginPage;
+import pageObjects.SwitchTabs;
+
+public class TestIndexFile {
+	
+	WebDriver driver;
+	LoginPage login;
+	SwitchTabs tabs;
+	FileContextMenu menu;
+	WebElement file;
+	
+	@Before
+	public void setUp() throws Exception{
+		driver = new FirefoxDriver(); 
+		login = new LoginPage(driver);
+		tabs = new SwitchTabs(driver);
+		menu = new FileContextMenu(driver);
+		
+		login.loginAdmin();
+		tabs.switchTabs("Documents");
+		file = driver.findElements(By.cssSelector("img[src='http://localhost:8080/skin/images/indexed.png']"))
+		.get(0);
+		menu.setFile(file);		
+	}
+	
+	@Test
+	public void testIndex() throws Exception {
+		menu.click("Index");
+	}
+	@After
+	public void tearDown(){
+		driver.close();
+	}
+}
---
+++ src/seleniumtest/java/tests/fileActions/TestMarkIndexableFile.java
@@ -0,0 +1,45 @@
+package tests.fileActions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import pageObjects.FileContextMenu;
+import pageObjects.LoginPage;
+import pageObjects.SwitchTabs;
+
+public class TestMarkIndexableFile {
+	
+	WebDriver driver;
+	LoginPage login;
+	SwitchTabs tabs;
+	FileContextMenu menu;
+	WebElement file;
+	
+	@Before
+	public void setUp() throws Exception{
+		driver = new FirefoxDriver(); 
+		login = new LoginPage(driver);
+		tabs = new SwitchTabs(driver);
+		menu = new FileContextMenu(driver);
+		
+		login.loginAdmin();
+		tabs.switchTabs("Documents");
+		file = driver.findElements(By.cssSelector("img[src='http://localhost:8080/skin/images/indexed.png']"))
+		.get(0);
+		menu.setFile(file);		
+	}
+	
+	@Test
+	public void testMarkIndex() throws Exception {
+		menu.click("Mark indexable");
+	}
+	@After
+	public void tearDown(){
+		driver.close();
+	}
+}
