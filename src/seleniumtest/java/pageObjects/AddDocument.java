f42919c0702c6f91f2b55296eea230e512e69dc2
johannesh <johanneshoefener@gmail.com>
2016-03-09 12:46:33 +0100

    tried another way of waiting explicitly

--- src/seleniumtest/java/pageObjects/AddDocument.java
+++ src/seleniumtest/java/pageObjects/AddDocument.java
@@ -1,8 +1,9 @@
 package pageObjects;
 
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
 import org.openqa.selenium.*;
+import static org.junit.Assert.*;
+
 
 
 public class AddDocument {
@@ -15,6 +16,13 @@ public class AddDocument {
 		   // TODO wait fails at the moment 
 		    driver.manage().timeouts().implicitlyWait(14, TimeUnit.SECONDS);
 		    Thread.sleep(4000);
+		   
+		    for (int second = 0;; second++) {
+		    	if (second >= 15) fail("timeout");
+		    	try { if ("".equals(driver.findElement(By.cssSelector("div.cancel")).getText())) break; } catch (Exception e) {}
+		    	Thread.sleep(1000);
+		    }
+		    
 		    driver.findElement(By.xpath("//div[11]/div[2]/div/div/div[3]/table/tbody/tr/td/div")).click(); //send file	
 			driver.findElement(By.id("isc_EJ")).click(); //save file
 		}
