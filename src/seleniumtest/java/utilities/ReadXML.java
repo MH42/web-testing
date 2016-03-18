1e082dbec20e93892db49fa5d210dcda0d9f12b4
johannesh <johanneshoefener@gmail.com>
2016-03-18 21:07:44 +0100

    Updated ReadXML

--- src/seleniumtest/java/utilities/ReadXML.java
+++ src/seleniumtest/java/utilities/ReadXML.java
@@ -35,20 +35,14 @@ public class ReadXML {
 		Document doc = dBuilder.parse(fXmlFile);
 		doc.getDocumentElement().normalize();
 
-		System.out.println("name,time,error,failure \n");
-
 		NodeList nList = doc.getElementsByTagName("testsuite");
 
-		int z = 0;
-		int x = 0;
-		int i = 0;
-		
 		String outputFile = System.getProperty("user.dir")+File.separator + file + ".csv";
 		FileWriter writer = new FileWriter(outputFile);
 		writer.append("name,time,error,failure");
 		writer.append('\n');
-		
-		for (i = 0; i < nList.getLength(); i++) {
+
+		for (int i = 0; i < nList.getLength(); i++) {
 
 			Node nNode = nList.item(i);
 
@@ -56,39 +50,27 @@ public class ReadXML {
 
 				Element eElement = (Element) nNode;
 
-				System.out.print(eElement.getAttribute("name"));
 				writer.append(eElement.getAttribute("name"));
 				writer.append(',');
-				System.out.print("\t" + eElement.getAttribute("time"));
 				writer.append(eElement.getAttribute("time"));
 				writer.append(',');				
 				if(	(eElement.getElementsByTagName("error")).getLength() > 0){
-					z++;
-					System.out.print("\t1");
 					writer.append("1");
 				} else
-					System.out.print("\t0");
 					writer.append("0");
-
-				
 				writer.append(',');	
-				
+
 				if(	(eElement.getElementsByTagName("failure")).getLength() > 0){
-					x++;
-					System.out.println("\t1");
 					writer.append("1");
 				} else
-					System.out.println("\t0");	
 					writer.append("0");
-				
-				 writer.append('\n');
+				writer.append('\n');
 
 			}
 		}
-		System.out.println(z + " " + x + " " + i);
-	    writer.flush();
-	    writer.close();
-	}	
+		writer.flush();
+		writer.close();
+}	
 
 
 
